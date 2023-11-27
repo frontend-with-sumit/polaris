0a3a9e5843eed264dcf6a415103fec53f237a33a
Sumit <sk990005@outlook.com>
2023-11-27 17:37:05 +0530

    add routing

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+	return (
+		<div>
+			<Link to="/visualize">Go to dashboard</Link>
+		</div>
+	);
+};
+
+export default Home;
