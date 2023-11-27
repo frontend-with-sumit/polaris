ab87f99aeff82fc229843c66311f7a67cc6a33d6
Sumit <sk990005@outlook.com>
2023-11-27 23:28:37 +0530

    add header

---
+++ src/components/Dashboard/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Profile from "./Profile";
+
+const Header = () => {
+	return (
+		<header className="bg-white py-4 px-8 shadow-sm flex justify-end">
+			<Profile />
+		</header>
+	);
+};
+
+export default Header;
